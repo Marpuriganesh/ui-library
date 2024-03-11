2e68b2c2e25cea3322f22c1efe310cbb7a725028
marpuri ganesh <marpuriganesh@gmail.com>
2024-03-11 14:05:35 +0530

    feat: commit test2

--- src/stories/AnimatedWave.stories.ts
+++ src/stories/AnimatedWave.stories.ts
@@ -9,13 +9,13 @@ const meta:Meta<typeof AnimatedWave> = {
         // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
         layout: "centered",
         backgrounds: {
-            default: 'twitter',
+            default: 'nice_blue',
             values: [
-              { name: 'twitter', value: '#00aced' },
-              { name: 'facebook', value: '#3b5998' },
+              { name: 'nice_blue', value: '#00aced' },
+              { name: 'dark_blue', value: '#3b5998' },
             ],
           },
-        
+
     },
     // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
     tags: ["autodocs"],
