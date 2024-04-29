ed577f3f1c0e1eefbfd893f23b813309f54a9635
marpuri ganesh <marpuriganesh@gmail.com>
2024-04-29 17:38:36 +0530

    feat(emotion-filter): basic

--- src/components/EmotionFilter/EmotionFilter.tsx
+++ src/components/EmotionFilter/EmotionFilter.tsx
@@ -1,17 +1,32 @@
 import React, { ChangeEvent, useState, useRef, useEffect } from "react";
 import "./EmotionFilter.css";
+import axios from "axios";
+import {motion} from 'framer-motion';
 
-const EmotionFilter: React.FC = () => {
+interface FeelingItem {
+  id: number;
+  feeling: string;
+  created_at: string;
+  updated_at: string;
+}
+
+interface Props {
+  url: string;
+}
+
+const EmotionFilter: React.FC<Props> = ({ url }) => {
   const [inputValue, setInputValue] = useState("");
   const [pointer, setPointer] = useState<"none" | "stroke" | "auto">("none");
   const inputRef = useRef<HTMLInputElement>(null);
+  const [id, setId] = useState(0);
+  const [feelings, setFeelings] = useState<FeelingItem[]>([]);
 
   useEffect(() => {
     if (inputRef.current) {
       inputRef.current.addEventListener("keydown", (event) => {
-        if (event.key === "Tab") {
+        if (event.key === " ") {
           event.preventDefault();
-          console.log("Tab key was pressed!");
+          console.log("SpaceBar key was pressed!");
           setInputValue("apple");
           // Perform your desired action here
         }
@@ -25,26 +40,72 @@ const EmotionFilter: React.FC = () => {
         });
       };
     }
+
+    // Add a return statement here
+    return undefined;
   }, []);
+
+
+
+ 
+
+
+
+
+  
+
+  const fetchEmotions = async (query: string): Promise<FeelingItem[]> => {
+    try {
+      const response = await axios.get(`${url}?q=${query}`);
+      const resultsArray = response.data.results;
+      // console.log(resultsArray); // Log the array of results
+
+      return resultsArray; // Return the array of results
+    } catch (error) {
+      console.error("Error fetching feelings:", error);
+      throw error; // Re-throw the error to propagate it further if needed
+    }
+  };
 
   function handleChange(event: ChangeEvent<HTMLInputElement>) {
     const value = event.target.value.toLowerCase();
     setInputValue(value);
+
+    if (value.length > 0) {
+      fetchEmotions(value)
+        .then((resultsArray) => {
+          setFeelings(resultsArray);
+        })
+        .catch((error) => {
+          console.error("Error fetching emotions:", error);
+          // Handle error if needed
+        });
+    } else {
+      setFeelings([]);
+    }
   }
 
   function handleOnFocus() {
-    console.log("focus");
+    // console.log("focus");
     setPointer("stroke");
   }
 
   function handleOnBlur() {
-    console.log("blur");
+    // console.log("blur");
+    // console.log(feelings);
     setPointer("none");
   }
 
   return (
     <>
       <div className="EmotionFilter">
+        <span
+          style={{
+            color: inputValue.length > 0 ? "white" : "#ffffff62",
+          }}
+        >
+          #
+        </span>
         <div className="total_container">
           <input
             type="text"
@@ -55,23 +116,18 @@ const EmotionFilter: React.FC = () => {
             onBlur={handleOnBlur}
             value={inputValue}
             ref={inputRef}
+            placeholder="feeling"
           />
-          <div className="inner_container" style={{ pointerEvents: pointer }}>
-            <div className="item">
-              <label>apple</label>
-            </div>
-            <div className="item">
-              <label>ball</label>
-            </div>
-            <div className="item">
-              <label>cat</label>
-            </div>
-            <div className="item">
-              <label>dog</label>
-            </div>
-            <div className="item">
-              <label>elephant</label>
-            </div>
+          <div
+            className="inner_container"
+            style={{ pointerEvents: pointer }}
+
+          >
+            {feelings.map((feeling, id) => (
+              <motion.div className="item" key={id} >
+                <label>{feeling.feeling}</label>
+              </motion.div>
+            ))}
           </div>
         </div>
       </div>
--- src/stories/EmotionFilter.stories.ts
+++ src/stories/EmotionFilter.stories.ts
@@ -23,5 +23,5 @@ export default meta;
 type story = StoryObj<typeof EmotionFilter>;
 
 export const EmotionFilterStory: story = {
-  args: {},
+  args: { url: "http://192.168.29.245:8000/api/feelings/" },
 };
