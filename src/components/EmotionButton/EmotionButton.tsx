717540d35dcb7688b429c9829d0c2ed835e8e69a
marpuriganesh <marpuriganesh@gmail.com>
2024-06-13 16:28:46 +0530

    fix(EmotionButton): sad emotion type

--- src/components/EmotionButton/EmotionButton.tsx
+++ src/components/EmotionButton/EmotionButton.tsx
@@ -32,7 +32,7 @@ const EmotionImages: EmotionImagesType = {
 };
 
 interface EmotionButtonProps {
-  emotion_value?: "😊" | "😊" | "😡" | "😮" | "🤢" | "😨";
+  emotion_value?: "😊" | "😢" | "😡" | "😮" | "🤢" | "😨";
   className?: string;
   setEmotion?: (value: number) => void;
   useLabel?: boolean;
