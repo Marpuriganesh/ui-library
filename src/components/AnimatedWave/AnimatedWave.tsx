f4cf74c7178f9d2284fdd2bcbaaed8ff864608aa
marpuriganesh <marpuriganesh@gmail.com>
2024-06-26 14:38:33 +0530

    feat(AnimatedWave): animations

--- src/components/AnimatedWave/AnimatedWave.tsx
+++ src/components/AnimatedWave/AnimatedWave.tsx
@@ -1,20 +1,39 @@
-import React, { useRef, useState, useEffect, ReactNode } from 'react';
-import './AnimatedWave.css';
-import throttle from 'lodash/throttle';
+import React, { useRef, useState, useEffect, ReactNode } from "react";
+import "./AnimatedWave.css";
+import throttle from "lodash/throttle";
+import { motion, MotionProps, AnimatePresence } from "framer-motion";
 
-interface AnimatedWaveProps {
-    children?:ReactNode;
-    phase?: number;
-    amplitude?: number;
-    speed?: number;
-    frequency?: number;
-    className?: string;
-    fill?: string;
+type MotionPropsWithOutStyle = Omit<MotionProps, "style">;
+
+interface AnimatedWaveProps extends MotionPropsWithOutStyle {
+  children?: ReactNode;
+  phase?: number;
+  amplitude?: number;
+  speed?: number;
+  frequency?: number;
+  className?: string;
+  fill?: string;
+
+  showWave?: boolean;
 }
 
-const AnimatedWave: React.FC<AnimatedWaveProps> = ({ phase = 10, amplitude = 60, speed = 10, frequency = 0.0005, className='',fill,children}) => {
+const AnimatedWave: React.FC<AnimatedWaveProps> = ({
+  phase = 10,
+  amplitude = 60,
+  speed = 10,
+  frequency = 0.0005,
+  className = "",
+  fill,
+  children,
+
+  showWave = true,
+  ...props
+}) => {
   const myDivRef = useRef<HTMLDivElement>(null);
-  const [dimensions, setDimensions] = useState<{ width: number; height: number }>({ width: 0, height: 0 });
+  const [dimensions, setDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
   const [value, setValue] = useState<number>(0);
 
   useEffect(() => {
@@ -25,10 +44,10 @@ const AnimatedWave: React.FC<AnimatedWaveProps> = ({ phase = 10, amplitude = 60,
       }
     };
 
-    window.addEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, []);
 
@@ -52,22 +71,37 @@ const AnimatedWave: React.FC<AnimatedWaveProps> = ({ phase = 10, amplitude = 60,
   }, [speed]);
 
   const wavePath = (x: number): string => {
-    const y = dimensions.height / 1.5 + amplitude * Math.sin(2 * Math.PI * frequency * x + (phase + value));
+    const y =
+      dimensions.height / 1.5 +
+      amplitude * Math.sin(2 * Math.PI * frequency * x + (phase + value));
     return `${x} ${y}`;
   };
 
-  const points = Array.from({ length: dimensions.width }, (_, i) => wavePath(i)).join(' ');
+  const points = Array.from({ length: dimensions.width }, (_, i) =>
+    wavePath(i)
+  ).join(" ");
 
   return (
-    <div style={{ display: 'inline-block' }} ref={myDivRef} className={(className===''?'animated-wave':className)} aria-label="Animated wave" >
-      <svg width={dimensions.width} height={dimensions.height}>
-        {children}
-        <path d={`M0 ${dimensions.height} ${points} L${dimensions.width} ${dimensions.height}`} fill={fill || '#000000'} />
-      </svg>
-    </div>
+    <AnimatePresence>
+      {showWave && (
+        <motion.div
+          style={{ display: "inline-block" }}
+          ref={myDivRef}
+          className={`${className === "" ? "animated-wave" : className} `}
+          aria-label="Animated wave"
+          {...props}
+        >
+          <motion.svg width={dimensions.width} height={dimensions.height}>
+            {children}
+            <path
+              d={`M0 ${dimensions.height} ${points} L${dimensions.width} ${dimensions.height}`}
+              fill={fill || "#000000"}
+            />
+          </motion.svg>
+        </motion.div>
+      )}
+    </AnimatePresence>
   );
 };
-
-
 
 export default AnimatedWave;
